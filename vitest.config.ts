5fda52123b7eefe4749b18ab680df87e191716bc
Harlan Wilton <harlan@harlanzw.com>
2022-03-29 02:38:13 +1100

    chore: mono clean up

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,13 +5,6 @@ import { defineConfig } from 'vite'
 const r = (p: string) => resolve(__dirname, p)
 
 export const alias: AliasOptions = {
-  'unrouted': r('./packages/unrouted/src/'),
-  '@unrouted/core': r('./packages/core/src/'),
-  '@unrouted/test-kit': r('./packages/test-kit/src/'),
-  '@unrouted/fetch': r('./packages/fetch/src/'),
-  '@unrouted/plugins': r('./packages/plugins/src/'),
-  '@unrouted/preset-api': r('./packages/preset-api/src/'),
-  '@unrouted/preset-node': r('./packages/preset-node/src/'),
 }
 
 export default defineConfig({
