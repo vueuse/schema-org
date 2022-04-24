a1883aae7b5e801d2a272b4c591bd1c2770e0194
Harlan Wilton <harlan@harlanzw.com>
2022-04-24 18:52:43 +1000

    fix: date utc snapshot

--- packages/schema-org/defineVideo/index.test.ts
+++ packages/schema-org/defineVideo/index.test.ts
@@ -9,7 +9,7 @@ describe('defineVideo', () => {
       useSchemaOrg([
         defineVideo({
           name: 'My cool video',
-          uploadDate: new Date(2020, 10, 10),
+          uploadDate: new Date(Date.UTC(2020, 10, 10)),
           url: '/image.png',
         }),
       ])
@@ -22,7 +22,7 @@ describe('defineVideo', () => {
             "@id": "https://example.com/#video/1656904464",
             "@type": "VideoObject",
             "name": "My cool video",
-            "uploadDate": "2020-11-09T13:00:00.000Z",
+            "uploadDate": "2020-11-10T00:00:00.000Z",
             "url": "https://example.com/image.png",
           },
         ]
