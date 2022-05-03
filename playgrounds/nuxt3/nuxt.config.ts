3420cbd8dcc5672afdce0660b6f579e2e706a4b7
Harlan Wilton <harlan@harlanzw.com>
2022-05-04 02:21:29 +1000

    chore: nuxt playground alias

--- playgrounds/nuxt3/nuxt.config.ts
+++ playgrounds/nuxt3/nuxt.config.ts
@@ -1,7 +1,14 @@
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
 import { defineNuxtConfig } from 'nuxt'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  alias: {
+    'nuxt-schema-org': resolve(__dirname, '../../packages/nuxt/src/module.ts'),
+  },
   modules: [
     'nuxt-windicss',
     'nuxt-schema-org',
