04e8c82f750045440ce1461b26c77179f8370037
Harlan Wilton <harlan@harlanzw.com>
2022-05-04 15:37:07 +1000

    chore: improved nuxt example

--- playgrounds/nuxt3/app.vue
+++ playgrounds/nuxt3/app.vue
@@ -1,11 +1,18 @@
 <script lang="ts" setup>
 useSchemaOrg([
-  defineOrganization({
+  defineLocalBusiness({
     name: 'Harlan\'s Hamburgers',
     logo: 'https://emojiguide.org/images/emoji/n/3ep4zx1jztp0n.png',
-    sameAs: [
-      'https://twitter.com/harlan_zw',
-    ],
+    address: {
+      streetAddress: '123 Main St',
+      addressLocality: 'Harlan',
+      addressRegion: 'MA',
+      postalCode: '01234',
+      addressCountry: 'US',
+    },
+    image: 'https://emojiguide.org/images/emoji/n/3ep4zx1jztp0n.png',
+    telephone: '+1-781-555-1212',
+    priceRange: '$',
   }),
   defineWebPagePartial(),
   defineWebSite({
@@ -17,6 +24,13 @@ useSchemaOrg([
     ],
   }),
 ])
+
+useHead({
+  title: 'Harlan\'s Hamburgers',
+  link: [
+    { rel: 'icon', type: 'image/png', href: 'https://emojiguide.org/images/emoji/n/3ep4zx1jztp0n.png' }
+  ],
+})
 const nav = [
   { name: 'Home', item: '/' },
   { name: 'About', item: '/about' },
@@ -26,8 +40,9 @@ const nav = [
 ]
 </script>
 <template>
-<div class="bg-blue-50 h-screen flex pb-20 flex-col items-center justify-center">
-  <div class="mb-20 gap-5 container mx-auto">
+<div class="bg-blue-50 flex pb-20 flex-col items-center justify-center">
+  <div class="mb-20 mt-5 gap-5 container mx-auto">
+    <h1 class="mb-3 text-xl">Harlan's Hamburgers 🍔</h1>
     <template v-for="(link, key) in nav" :key="key">
     <NuxtLink :to="link.item" class="underline mr-5">
       {{ link.name }}
@@ -37,11 +52,8 @@ const nav = [
   </div>
   <div class="container mx-auto flex items-center gap-20">
     <div class="w-full max-h-900px overflow-y-auto ">
-      <h1 class="text-7xl mb-20">
-        {{ $route.meta.title }}
-      </h1>
       <div>
-      <NuxtPage />
+        <NuxtPage />
       </div>
     </div>
     <SchemaOrgInspector />
--- playgrounds/nuxt3/pages/index.vue
+++ playgrounds/nuxt3/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 definePageMeta({
-  description: 'My page description',
+  description: 'High quality hamburgers delivered fresh to you.',
 })
 useHead({
   title: 'Harlan Hamburgers 🍔',
@@ -8,6 +8,16 @@ useHead({
 </script>
 <template>
   <div>
-    <strong>{{ $route.meta.description }}</strong>
+    <h2 class="text-3xl mb-5">
+      Money can't buy happiness, but it can buy Harlan's Hamburgers.
+    </h2>
+    <div class="mb-5">🍔🍔🍔🍔🍔🍔🍔🍔🍔🍔🍔🍔🍔🍔🍔🍔</div>
+    <div class="text-xl font-light mb-5">
+      <p class="mb-3">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam, aspernatur consequuntur corporis debitis eaque eveniet incidunt inventore provident quam, quas quasi tempore vel! Dolorum est pariatur quas quos sint?
+      </p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aspernatur, at beatae debitis, dolores eum eveniet hic maiores minus optio quaerat sunt vel voluptate? Cupiditate quas quidem reiciendis tempora velit?</p>
+    </div>
+
   </div>
 </template>
