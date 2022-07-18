c9d71618074c7858259537cec9305397c336c19a
Harlan Wilton <harlan@harlanzw.com>
2022-07-18 12:27:13 +1000

    fix(SchemaOrgInspector): remove syntax highlighting for better reactivity

--- packages/schema-org/components/SchemaOrgInspector/index.ts
+++ packages/schema-org/components/SchemaOrgInspector/index.ts
@@ -1,27 +1,6 @@
-import {computed, defineComponent, h, nextTick, ref, watch} from 'vue'
+import { defineComponent, h, ref, watch } from 'vue'
 import { injectSchemaOrg } from '../../useSchemaOrg'
 import type { SchemaOrgClient } from '../../types'
-
-function simpleJSONSyntaxHighlighter(json: string) {
-  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
-  json = json.replace(/"@type": "(.*?)"/gm, '"@type": "<a target=\'_blank\' href=\'https://schema.org/$1\'>$1</a>"')
-  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, (match) => {
-    let cls = 'number'
-    if (/^"/.test(match)) {
-      if (/:$/.test(match))
-        cls = 'key'
-      else
-        cls = 'string'
-    }
-    else if (/true|false/.test(match)) {
-      cls = 'boolean'
-    }
-    else if (/null/.test(match)) {
-      cls = 'null'
-    }
-    return `<span class="${cls}">${match}</span>`
-  })
-}
 
 export const SchemaOrgInspector = defineComponent({
   name: 'SchemaOrgInspector',
@@ -31,7 +10,7 @@ export const SchemaOrgInspector = defineComponent({
       default: false,
     },
   },
-  async setup(props) {
+  setup(props) {
     let client: undefined | SchemaOrgClient
     try {
       client = injectSchemaOrg()
@@ -40,60 +19,38 @@ export const SchemaOrgInspector = defineComponent({
     if (!client) {
       // never resolves, never hydrates
       return () => {
-        return new Promise(() => {})
+        return h('div', {}, 'Failed to find schemaOrg client.')
       }
     }
 
-    const schema = ref(client.schemaRef.value)
+    const schemaRaw = ref('')
 
     watch(client.schemaRef, (val) => {
-      schema.value = val
+      schemaRaw.value = val
+
+      if (props.console) {
+        // eslint-disable-next-line no-console
+        console.debug('[SchemaOrgInspector]', client?.graphNodes)
+      }
     }, {
-      deep: true,
-    })
-
-    if (props.console) {
-      // eslint-disable-next-line no-console
-      console.debug('[SchemaOrgInspector]', client.graphNodes)
-    }
-
-    const value = computed(() => {
-      return simpleJSONSyntaxHighlighter(schema.value)
-    })
-
-    // SSR does not have reactivity, we should wait until other components can render
-    await new Promise<void>((resolve) => {
-      nextTick(() => {
-        schema.value = client?.schemaRef.value || ''
-        resolve()
-      })
+      immediate: true,
     })
 
     return () => {
       return h('div', {
+        class: ['schema-org-inspector'],
         style: {
-          display: 'inlineBlock',
+          backgroundColor: '#282839',
+          color: '#c5c6c9',
+          padding: '5px',
+          borderRadius: '5px',
+          width: '900px',
+          height: '600px',
+          overflowY: 'auto',
+          boxShadow: '3px 4px 15px rgb(0 0 0 / 10%)',
         },
       }, [
-        h('style', '.schema-org-inspector .string { color: #7ec9a5; }\n'
-          + '.schema-org-inspector .number { color: #3ca0c8; }\n'
-          + '.schema-org-inspector a { color: #7ec9a5; text-decoration: underline; }\n'
-          + '.schema-org-inspector .boolean { color: #3ca0c8; }\n'
-          + '.schema-org-inspector .null { color: #3ca0c8; }\n'
-          + '.schema-org-inspector .key { color: #9fb5f5; }'),
-        h('div', {
-          class: ['schema-org-inspector'],
-          style: {
-            backgroundColor: '#282839',
-            color: '#c5c6c9',
-            padding: '5px',
-            borderRadius: '5px',
-            maxWidth: '900px',
-            maxHeight: '600px',
-            overflowY: 'auto',
-            boxShadow: '3px 4px 15px rgb(0 0 0 / 10%)',
-          },
-        }, [h('pre', { style: { textAlign: 'left' }, innerHTML: value.value })]),
+        h('pre', { style: { textAlign: 'left' } }, schemaRaw.value),
       ])
     }
   },
