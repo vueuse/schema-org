51d7735c2681e47105ad5b00d3ca394599e4fd07
Harlan Wilton <harlan@harlanzw.com>
2022-07-19 16:39:42 +1000

    chore(playground): revert aliases

--- playgrounds/nuxt3/nuxt.config.ts
+++ playgrounds/nuxt3/nuxt.config.ts
@@ -1,16 +1,8 @@
 import { defineNuxtConfig } from 'nuxt'
-import { resolvePath } from 'mlly'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig(async () => {
-  const resolve = (s: string) => resolvePath(s, { url: import.meta.url })
-  const alias = {
-    'nuxt-schema-org': await resolve('nuxt-schema-org'),
-    // fix monorepo dependency conflicts
-    '@vueuse/schema-org': await resolve('@vueuse/schema-org'),
-  }
   return {
-    alias,
     modules: [
       'nuxt-windicss',
       'nuxt-schema-org',
