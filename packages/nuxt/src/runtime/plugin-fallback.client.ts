e11a0853ec20db0def49c285e6a924bb81679d15
Harlan Wilton <harlan@harlanzw.com>
2022-08-15 15:00:07 +1000

    fix(nuxt): avoid scanning payload for now

--- packages/nuxt/src/runtime/plugin-fallback.client.ts
+++ packages/nuxt/src/runtime/plugin-fallback.client.ts
@@ -2,13 +2,8 @@ import { defineNuxtPlugin } from '#app'
 import { watch } from '#imports'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  if (nuxtApp.payload.data?.schemaOrg) {
-    nuxtApp._useHead(nuxtApp.payload.data.schemaOrg)
-  }
-  else {
-    // remove script to avoid confusion
-    watch(() => nuxtApp._route.path, () => {
-      document.querySelector('script[data-id="schema-org-graph"]')?.remove()
-    })
-  }
+  // remove script to avoid confusion
+  watch(() => nuxtApp._route.path, () => {
+    document.querySelector('script[data-id="schema-org-graph"]')?.remove()
+  })
 })
