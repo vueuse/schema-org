15bb62e9b4bc21e73f7286367ce732341cc6ac78
Harlan Wilton <harlan@harlanzw.com>
2022-08-23 21:11:19 +1000

    fix(vite): infer meta from useHead

--- packages/vite/src/vite.ts
+++ packages/vite/src/vite.ts
@@ -3,11 +3,12 @@ import type { UserConfig } from '@vueuse/schema-org'
 import type { App } from 'vue'
 import { createHead } from '@vueuse/head'
 import type { Router } from 'vue-router'
+import type { HeadClient } from '@vueuse/head'
 
 export function installSchemaOrg(ctx: { app: App; router?: Router }, config: UserConfig) {
   const resolvedConfig = resolveUserConfig(config)
 
-  let head = ctx.app._context.provides.usehead
+  let head = ctx.app._context.provides.usehead as HeadClient
   if (!head) {
     head = createHead()
     ctx.app.use(head)
@@ -20,10 +21,24 @@ export function installSchemaOrg(ctx: { app: App; router?: Router }, config: Use
         head.updateDOM()
     },
     meta() {
-      // const inferredMeta: Record<string, any> = {}
+      const inferredMeta: Record<string, any> = {}
+
+      const tags = head.headTags?.reverse()
+      if (tags) {
+        const headTag = tags.filter(t => t.tag === 'title' && !!t.props.children)
+        if (headTag.length)
+          inferredMeta.title = headTag[0].props.children
+        const descTag = tags.filter(t => t.tag === 'meta' && t.props.name === 'description' && !!t.props.content)
+        if (descTag.length)
+          inferredMeta.description = descTag[0].props.content
+        const imageTag = tags.filter(t => t.tag === 'meta' && t.props.property === 'og:image' && !!t.props.content)
+        if (imageTag.length)
+          inferredMeta.image = imageTag[0].props.content
+      }
 
       return {
         path: ctx.router?.currentRoute.value.path || '/',
+        ...inferredMeta,
         ...resolvedConfig.meta,
         ...ctx.router?.currentRoute.value.meta || {},
       }
--- packages/vite/src/vitesse.ts
+++ packages/vite/src/vitesse.ts
@@ -16,15 +16,22 @@ export function installSchemaOrg(ctx: ViteSSGContext, config: UserConfig) {
     meta() {
       const inferredMeta: Record<string, any> = {}
 
-      const tags = ctx.head?.headTags
+      const tags = ctx.head?.headTags?.reverse()
       if (tags) {
-        const headTag = tags.reverse().filter(t => t.tag === 'title' && !!t.props.children)
+        const headTag = tags.filter(t => t.tag === 'title' && !!t.props.children)
         if (headTag.length)
           inferredMeta.title = headTag[0].props.children
+        const descTag = tags.filter(t => t.tag === 'meta' && t.props.name === 'description' && !!t.props.content)
+        if (descTag.length)
+          inferredMeta.description = descTag[0].props.content
+        const imageTag = tags.filter(t => t.tag === 'meta' && t.props.property === 'og:image' && !!t.props.content)
+        if (imageTag.length)
+          inferredMeta.image = imageTag[0].props.content
       }
 
       return {
         path: ctx.router.currentRoute.value.path,
+        ...inferredMeta,
         ...resolvedConfig.meta,
         ...ctx.router.currentRoute.value.meta,
       }
