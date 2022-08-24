1234a71bdd127cff04ebd3efdeed04c9944558c7
Harlan Wilton <harlan@harlanzw.com>
2022-08-24 17:32:16 +1000

    fix(types): use simpler type generics to avoid missing types

--- packages/schema-org/providers/simple.ts
+++ packages/schema-org/providers/simple.ts
@@ -70,12 +70,7 @@ import {
 import type { Ref } from 'vue'
 
 export type MaybeRef<T> = T | Ref<T>
-
-export type DeepMaybeRef<T> = T extends Ref<infer V>
-  ? MaybeRef<V>
-  : T extends Array<any> | object
-    ? { [K in keyof T]: DeepMaybeRef<T[K]> }
-    : MaybeRef<T>
+export type DeepMaybeRef<T> = { [K in keyof T]: MaybeRef<T[K]> }
 
 type Node<T> = Omit<DeepMaybeRef<T>, '@type'>
 
