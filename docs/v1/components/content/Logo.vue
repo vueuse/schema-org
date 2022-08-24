410b2a0838a227d6a2d15844e956791db090cc2c
Harlan Wilton <harlan@harlanzw.com>
2022-08-24 13:12:49 +1000

    chore(docs): migrate v1 docs to schema-org-graph-js

--- docs/v1/components/content/Logo.vue
+++
@@ -1,6 +0,0 @@
-<template>
-<div class="flex items-center">
-  <Icon name="logos:vueuse" class="mr-3 h-7 w-7 mt-1" />
-  <h1 class="font-bold"><span class="opacity-60 text-xs">@vueuse/</span><br>Schema.org</h1>
-</div>
-</template>
